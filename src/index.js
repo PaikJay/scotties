a8d4bc869e8907549e93ba69c135bcd2da7f857d
Paik Jay <51130958+PaikJay@users.noreply.github.com>
2022-06-29 10:01:03 -0400

    change basename in browser router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="https://paikjay.github.io/scotties/">
+    <BrowserRouter basename="/scotties">
     <App />
     </BrowserRouter>
   </React.StrictMode>
