5d0538b851b14e9e658b24cb368a86c6c0685856
Paik Jay <51130958+PaikJay@users.noreply.github.com>
2022-06-30 15:33:02 -0400

    test /scotties routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
  
   render() {
     return (
-      <Router basename="https://paikjay.github.io/scotties/"> 
+      <Router basename="/scotties"> 
       <div className="App">
         <Navigation />
 
